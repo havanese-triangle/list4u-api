c89099bdd12dc826c9a72a26a0cf0d04bc1ac415
Todd W Crone <twcrone@gmail.com>
2021-12-23 09:50:43 -0500

    Add postgres and local config

---
+++ runDockerPostgres.sh
@@ -0,0 +1,1 @@
+docker run -p 5432:5432 --name list4udb -e POSTGRES_PASSWORD=postgres -d postgres
--- src/main/groovy/com/havitri/list4u/api/Item.groovy
+++ src/main/groovy/com/havitri/list4u/api/Item.groovy
@@ -1,6 +1,15 @@
 package com.havitri.list4u.api
 
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
 class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id
+
     String name
 }

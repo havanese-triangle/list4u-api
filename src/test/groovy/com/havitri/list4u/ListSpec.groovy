fd89a1cf308c1a5f6912995cb344e57c03b86ee7
Todd W Crone <twcrone@gmail.com>
2021-12-23 09:06:45 -0500

    Remove all specs

---
+++ src/test/groovy/com/havitri/list4u/ListSpec.groovy
@@ -0,0 +1,19 @@
+package com.havitri.list4u
+
+import com.havitri.list4u.api.Item
+import spock.lang.Specification
+
+class ListSpec extends Specification {
+
+    def "nothing interesting"() {
+        given:
+        def item = new Item()
+
+        when:
+        item.id = 1
+        item.name = "Pizza"
+
+        then:
+        item.name == "Pizza"
+    }
+}

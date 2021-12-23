e79781357fad56c16d6b7855dcfe6b4be62ae291
Todd W Crone <twcrone@gmail.com>
2021-12-23 12:10:57 -0500

    Add default list if none exist yet

--- src/main/groovy/com/havitri/list4u/api/view/ListController.groovy
+++ src/main/groovy/com/havitri/list4u/api/view/ListController.groovy
@@ -4,10 +4,12 @@ package com.havitri.list4u.api.view
 import com.havitri.list4u.api.entity.ListEntity
 import com.havitri.list4u.api.entity.ListRepository
 import groovy.json.JsonGenerator
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Slf4j
 @RestController
 class ListController {
 
@@ -23,6 +25,10 @@ class ListController {
     String getList() {
         def lists = repository.findAll()
         def list = lists.empty ? new ListEntity(name: "Grocery") : lists[0]
+        if(!list.id) {
+            log.warn("No lists found adding default 'Grocery' list")
+            list = repository.save(list)
+        }
         def json = jsonGenerator.toJson(list)
         json
     }

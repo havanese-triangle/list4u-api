51343ffd80dac1678abb2977bcb6e23925cff025
twcrone <twcrone@gmail.com>
2021-12-29 11:17:06 -0500

    Add CORS support for all origins

--- src/main/java/com/havitri/list4u/api/List4UApi.java
+++ src/main/java/com/havitri/list4u/api/List4UApi.java
@@ -2,6 +2,8 @@ package com.havitri.list4u.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class List4UApi {
@@ -10,4 +12,12 @@ public class List4UApi {
         SpringApplication.run(List4UApi.class, args);
     }
 
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 }
